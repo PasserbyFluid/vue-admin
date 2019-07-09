ad072d40ab214d1207d21a02eb44bdd40236c38d
PasserbyFluid <406242283@qq.com>
2019-07-09 11:02:03 +0800

    delete useless files

--- src78/store/getters.js
+++
@@ -1,9 +0,0 @@
-const getters = {
-  sidebar: state => state.app.sidebar,
-  device: state => state.app.device,
-  todolist: state => state.app.todolist,
-  token: state => state.user.token,
-  avatar: state => state.user.avatar,
-  name: state => state.user.name
-}
-export default getters
